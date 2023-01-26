fb22af2a4f9a61ff4b2ff8c874be9dfa6065445f
Sebastian Muntean <munteansebastian@gmail.com>
2023-01-26 11:28:17 +0200

    Implement sorting.

--- src/main/java/com/sebastian/ems/controller/EmployeeController.java
+++ src/main/java/com/sebastian/ems/controller/EmployeeController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,7 +18,7 @@ public class EmployeeController {
 
     @GetMapping("/")
     public String viewHomePage(Model model) {
-        return findPaginated(1, model);
+        return findPaginated(1, "firstName", "asc", model);
     }
 
     @GetMapping("/newEmployeeForm")
@@ -51,14 +48,18 @@ public class EmployeeController {
     }
 
     @GetMapping("/page/{pageNo}")
-    public String findPaginated(@PathVariable (value = "pageNo") int pageNo, Model model) {
+    public String findPaginated(@PathVariable (value = "pageNo") int pageNo, @RequestParam("sortField") String sortField,
+                                @RequestParam("sortDir") String sortDir, Model model) {
         int pageSize = 5;
-        Page<Employee> page = employeeService.findPaginated(pageNo, pageSize);
+        Page<Employee> page = employeeService.findPaginated(pageNo, pageSize, sortField, sortDir);
         List<Employee> listEmployees = page.getContent();
 
         model.addAttribute("currentPage", pageNo);
         model.addAttribute("totalPages", page.getTotalPages());
         model.addAttribute("totalItems", page.getTotalElements());
+        model.addAttribute("sortField", sortField);
+        model.addAttribute("sortDir", sortDir);
+        model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");
         model.addAttribute("listEmployees", listEmployees);
 
         return "index";
